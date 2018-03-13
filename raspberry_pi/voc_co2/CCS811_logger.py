8d565900305c6e9090060b3fc1bc5ced0d730f89
Forrest Marshall <fspmarshall@gmail.com>
2018-03-13 10:33:06 -1000

    #11 added a filter to prevent storing biased data

--- raspberry_pi/voc_co2/CCS811_logger.py
+++ raspberry_pi/voc_co2/CCS811_logger.py
@@ -9,6 +9,7 @@ def write2csv(data):
 	log.writerow(data)
 	dataCSV.close()
 
+###### Initialization for the CCS811 sensor from the Adafruit example.py ######
 ccs =  Adafruit_CCS811()
 
 while not ccs.available():
@@ -16,6 +17,8 @@ while not ccs.available():
 temp = ccs.calculateTemperature()
 ccs.tempOffset = temp - 25.0
 
+
+#adds the file header for new csv
 file_exist = os.path.isfile('CCS811_log.csv')
 if not file_exist:
 	write2csv(['Timestamp', 'CO2 (ppm)', 'TVOC (ppb)', 'Temperature (C)'])
@@ -27,8 +30,11 @@ while(1):
 	    temp = ccs.calculateTemperature()
 	    timestamp = time()
 	    if not ccs.readData():
-	      print "timestamp: ", timestamp, ", CO2: ", CO2, "ppm, TVOC: ", TVOC, " temp: ", temp
-	      write2csv([timestamp, CO2, TVOC, temp])
+	      if CO2 == 0:
+	        pass
+	      else:
+	        print "timestamp: ", timestamp, ", CO2: ", CO2, "ppm, TVOC: ", TVOC, " temp: ", temp
+	        write2csv([timestamp, CO2, TVOC, temp])
 	    else:
 	      print "ERROR!"
 	      while(1):
