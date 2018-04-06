33e79bd8a581cb850aed6102472caffaba732e5b
jygh98 <jyim3@hawaii.edu>
2018-04-06 10:36:11 -1000

    #14 replaced magic numbers with symbolic constants

--- raspberry_pi/pms5003/PMS5003_logger.py
+++ raspberry_pi/pms5003/PMS5003_logger.py
@@ -4,6 +4,32 @@ import struct
 import os
 import csv
 
+# Default serial port for raspberry pi 2
+PI2_SERIALPORT = "/dev/ttyAMA0"
+# Baudrate specified for the PMS5003 sensor
+PMS5003_BAUDRATE = 9600
+
+# Fixed flag bytes
+START_FLAG1 = b'\x42'
+START_FLAG2 = b'\x4d'
+
+# Data is read in the order from high to low (big-endian format)
+BYTE_ORDER = ">"
+# Convert from bytes to integer for all 14 data values
+FORMAT_CHARACTER = "HHHHHHHHHHHHHH"
+
+# Normal frame length is 32 bytes
+FRAME_LENGTH = 32
+# Frame length excluding data 14 high and data 14 low
+CHECKSUM_FRAME_LENGTH = 30
+
+# Index of start flag 1 byte
+BYTE_1 = 0
+# Index of start flag 2 byte
+BYTE_2 = 1
+# Index of the first data byte to be unpacked
+BYTE_5 = 4
+
 def csvwrite(data):
     dataCSV = open('pi_pms5003_log.csv', 'a')
     log = csv.writer(dataCSV)
@@ -11,21 +37,20 @@ def csvwrite(data):
     dataCSV.close()
 
 def main():
-    # "/dev/ttyAMA0" is the serial port for the raspberry pi 2
-    # 9600 is the default baudrate for the PMS5003 sensor
-    # default value of 2 second timeout
-    ser = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=2.0) # open serial port
+    # open serial port
+    ser = serial.Serial(PI2_SERIALPORT, baudrate=PMS5003_BAUDRATE, timeout=2.0)
     # frame length is 32 bytes
-    s = ser.read(32)
-    # check first start byte is 0x42 and second start byte is 0x4d
-    if s[0] == b'\x42' and s[1] == b'\x4d':
+    s = ser.read(FRAME_LENGTH)
+    # check if the start flags matches
+    if s[BYTE_1] == START_FLAG1 and s[BYTE_2] == START_FLAG2:
         # data returned from the serial line must be converted from two 8 bit high and low to single 16 bit integer
-        data = struct.unpack(">HHHHHHHHHHHHHH", bytes(s[4:]))
+        data = struct.unpack(BYTE_ORDER+FORMAT_CHARACTER, bytes(s[BYTE_5:]))
         # order of the unpacking is according to the transport protocol Active mode in the datasheet
         pm1_standard, pm25_standard, pm10_standard, pm1_env, pm25_env, pm10_env, particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um, skip, checksum = data
-        # compare the checksum byte to the sum of the fram values from s[0] + s[1] + ... + s[29]
+        # compare the checksum byte to the sum of each byte of the frame excluding the checksum bytes
+        # i.e checksum = byte1 + byte2 + ... + byte30
         check = 0
-        for i in range(30):
+        for i in range(CHECKSUM_FRAME_LENGTH):
             check += ord(s[i])
         if check == checksum:
             print "Concentration Units (ug/m^3)"
