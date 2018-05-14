4685566b68e4c5fa6708a29ea9da7e70182c94ce
jygh98 <jyim3@hawaii.edu>
2018-05-14 13:35:56 -1000

    #31 added csv logging

--- raspberry_pi/si7021/Si7021_logger.py
+++ raspberry_pi/si7021/Si7021_logger.py
@@ -1,5 +1,15 @@
 import pigpio
-import time
+from time import sleep, time
+import csv
+import os
+
+OUTPUT_FILE = 'pi_si7021_log.csv'
+
+def csvwrite(data):
+    dataCSV = open(OUTPUT_FILE, 'a')
+    log = csv.writer(dataCSV)
+    log.writerow(data)
+    dataCSV.close()
 
 # I2C Address for the Si7021
 I2C_ADDRESS = 0x40
@@ -23,19 +33,31 @@ def temperature(raw):
 pi = pigpio.pi()
 device = pi.i2c_open(BUS, I2C_ADDRESS)
 
-while True:
+
+def main():
     device = pi.i2c_write_byte(device, CMD_RH)
-    time.sleep(0.1)
+    sleep(0.1)
     a, b = pi.i2c_read_device(device, 2)
     raw_humidity = int16bit(b)
     humidity = relative_humidity(raw_humidity)
     print("Humidity = " + str(round(humidity, 3)))
-    
-    
+
     pi.i2c_write_byte(device, CMD_TEMP)
     c, d = pi.i2c_read_device(device, 2)
     raw_temp = int16bit(d)
     temp = temperature(raw_temp)
     print("Temperature = " + str(round(temp, 3)))
-    
-    time.sleep(1)
+
+    csvwrite([timestamp, humidity, temp])
+
+
+if __name__ == "__main__":
+    try:
+        if not os.path.isfile(OUTPUT_FILE):
+            csvwrite(['timestamp', 'humidity_percent', 'temperature_c'])
+        while True:
+            main()
+            sleep(2)
+
+    except KeyboardInterrupt:
+        pass
