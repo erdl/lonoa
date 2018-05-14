60baf8b1b916ea2fa74820b19cba7eecfb127ead
jason <jyim3@hawaii.edu>
2018-05-14 23:31:31 +0000

    #31 added symbolic constants

--- raspberry_pi/si7021/Si7021_logger.py
+++ raspberry_pi/si7021/Si7021_logger.py
@@ -1,32 +1,41 @@
 import pigpio
 import time
 
-pi = pigpio.pi()
+# I2C Address for the Si7021
+I2C_ADDRESS = 0x40
+# Access Bus 1 on the Raspberry Pi
+BUS = 1
 
-device = pi.i2c_open(1, 0x40)
+# Si7021 Commands for Taking Measurements
+CMD_RH = 0xF5
+CMD_TEMP = 0xE0
+
+# Convert a two bytes string into a 16 bit integer
+def int16bit(b):
+    return(b[0] << 8) + (b[1])
+
+def relative_humidity(raw):
+    return raw * 125.0 / 65536.0 - 6.0
+
+def temperature(raw):
+    return raw * 175.72 / 65536.0 - 46.85
+
+pi = pigpio.pi()
+device = pi.i2c_open(BUS, I2C_ADDRESS)
 
 while True:
-    device = pi.i2c_write_byte(device, 0xF5)
+    device = pi.i2c_write_byte(device, CMD_RH)
     time.sleep(0.1)
     a, b = pi.i2c_read_device(device, 2)
-
-    hum_LSB = b[0] << 8
-    humidity = hum_LSB + b[1]
-    humidity = humidity * 125
-    humidity = humidity / 65536
-    humidity = humidity - 6
-
-    print("Humidity = " + str(humidity))
-
-    pi.i2c_write_byte(device, 0xE0)
+    raw_humidity = int16bit(b)
+    humidity = relative_humidity(raw_humidity)
+    print("Humidity = " + str(round(humidity, 3)))
+    
+    
+    pi.i2c_write_byte(device, CMD_TEMP)
     c, d = pi.i2c_read_device(device, 2)
-
-    temp_MSB = d[0] << 8
-    temperature = temp_MSB + d[1]
-    temperature = temperature * 172.72
-    temperature = temperature / 65536
-    temperature = temperature - 46.85
-
-    print("Temperature = " + str(round(temperature, 2)))
-
+    raw_temp = int16bit(d)
+    temp = temperature(raw_temp)
+    print("Temperature = " + str(round(temp, 3)))
+    
     time.sleep(1)
