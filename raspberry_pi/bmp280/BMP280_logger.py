8e7e43575dea4afc467c9a92950acaa38bbafc02
jason <jyim3@hawaii.edu>
2018-05-16 21:05:37 +0000

    #16 added data averaging function

--- raspberry_pi/bmp280/BMP280_logger.py
+++ raspberry_pi/bmp280/BMP280_logger.py
@@ -8,6 +8,10 @@ I2C_ADDRESS = 0x76
 
 OUTPUT_FILE = 'pi_bmp280_log.csv'
 
+# Number of readings to take before calculating the average
+AVERAGE_READ = 300
+# Fieldset of keys used to parse the data list to calculate the average
+AVERAGE_FIELD = ['data1', 'data2', 'data3', 'data4']
 
 def csvwrite(data):
     dataCSV = open(OUTPUT_FILE, 'a')
@@ -25,29 +29,47 @@ def getsealevel_pa():
         # default value for sea level pressure
         return 101630.0
 
+def average(data_list):
+    avg_data = []
+    avg_data.append(time())
+    for key in AVERAGE_FIELD:
+        avg_data.append(float((sum(data[key] for data in data_list)) / len(data_list)))
+    return avg_data
 
 def main():
-    sensor = BMP280(mode=BMP280_ULTRAHIGHRES,address=I2C_ADDRESS)
+    data_set = []
+    while True:
+        sensor = BMP280(mode=BMP280_ULTRAHIGHRES,address=I2C_ADDRESS)
 
-    temp = sensor.read_temperature()
-    altitude = sensor.read_altitude(sealevel_pa=getsealevel_pa())
-    # pressure is returned in units of pascals and we would like to convert to hectopascal
-    atmospheric_pressure = sensor.read_pressure() / 100
-    sealevel_pressure = sensor.read_sealevel_pressure(altitude_m=altitude) / 100
+        temp = sensor.read_temperature()
+        altitude = sensor.read_altitude(sealevel_pa=getsealevel_pa())
+        # pressure is returned in units of pascals and we would like to convert to hectopascal
+        atmospheric_pressure = sensor.read_pressure() / 100
+        sealevel_pressure = sensor.read_sealevel_pressure(altitude_m=altitude) / 100
+        data = {'data1': temp,
+                'data2': altitude,
+                'data3': atmospheric_pressure,
+                'data4': sealevel_pressure }
+        data_set.append(data)
 
-    print 'Temp      = {0:0.1f} deg C'.format(temp)
-    print 'Altitude  = {0:0.2f} meters'.format(altitude)
-    print 'Atmospheric Pressure  = {0:0.2f} hPa'.format(atmospheric_pressure)
-    print 'Sea-Level Pressure = {0:0.2f} hPa'.format(sealevel_pressure)
-    csvwrite([time(), temp, altitude, atmospheric_pressure, sealevel_pressure])
+        #print 'Temp      = {0:0.1f} deg C'.format(temp)
+        #print 'Altitude  = {0:0.2f} meters'.format(altitude)
+        #print 'Atmospheric Pressure  = {0:0.2f} hPa'.format(atmospheric_pressure)
+        #print 'Sea-Level Pressure = {0:0.2f} hPa'.format(sealevel_pressure)
+        
+        if len(data_set) >= AVERAGE_READ:
+            average_data = average(data_set)
+            timestamp, avg_temp, avg_altitude, avg_pressure, avg_sealvl_pressure = average_data
+            csvwrite([timestamp, avg_temp, avg_altitude, avg_pressure, avg_sealvl_pressure])
+            del data[:]
+        
+        sleep(1)
 
 if __name__ == "__main__":
     try:
         if not os.path.isfile(OUTPUT_FILE):
-            csvwrite(['timestamp', 'temperature_c', 'altitude_m', 'atmospheric-pressure_hPa', 'sealevel-pressure_hPa'])
-        while True:
-            main()
-            sleep(2)
+            csvwrite(['timestamp', 'temperature_c', 'altitude_m', 'atmospheric-pressure_hpa', 'sealevel-pressure_hpa'])
+        main()
 
     except KeyboardInterrupt:
         pass
