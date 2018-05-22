dd034afa1f4bd71ff5080f824e6b968745c93d3e
mschultz@hawaii.edu <mschultz@hawaii.edu>
2018-05-22 10:07:06 -1000

    #37 created shell script with simple test cases

---
+++ egauge/test_egauge_api_01.sh
@@ -0,0 +1,6 @@
+#using a workaround since default shell is /bin/sh not /bin/bash
+#python3 egauge_api.py 1526343315 1526548515 725 ~/Projects/output.csv
+#offline egauge
+#python3 egauge_api.py 1526548513 1526549515 34111 ~/Projects/egauge/output.csv
+#online egauge
+python3 egauge_api.py 1526548513 1526549515 725 ~/Projects/egauge/output.csv
